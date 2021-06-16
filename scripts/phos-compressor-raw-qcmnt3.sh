e1aaa3f677a7bbbb00de0e822f9d9ca715d27c3f
Barthélémy von Haller <barthelemy.von.haller@gmail.com>
2021-06-16 16:44:47 +0200

    Update scripts/phos-compressor-raw-qcmnt3.sh
    Co-authored-by: Piotr Konopka <piotr.jan.konopka@cern.ch>

--- scripts/phos-compressor-raw-qcmnt3.sh
+++ scripts/phos-compressor-raw-qcmnt3.sh
@@ -4,7 +4,7 @@
 set -e;
 set -u;
 
-WF_NAME=phos-compressor-raw-qcmn-localt3
+WF_NAME=phos-compressor-raw-qcmnt3-local
 QC_GEN_CONFIG_PATH='json://'`pwd`'/etc/phos-compressor-raw-qcmn.json'
 QC_FINAL_CONFIG_PATH='consul-json://{{ consul_endpoint }}/o2/components/qc/ANY/any/phos-compressor-raw-qcmn'
 QC_CONFIG_PARAM='qc_config_uri'
@@ -40,4 +40,3 @@ sed -i /defaults:/\ a\\\ \\\ "${QC_CONFIG_PARAM}":\ \""${ESCAPED_QC_FINAL_CONFIG
 # find and replace all usages of the QC config path which was used to generate the workflow
 ESCAPED_QC_GEN_CONFIG_PATH=$(printf '%s\n' "$QC_GEN_CONFIG_PATH" | sed -e 's/[]\/$*.^[]/\\&/g');
 sed -i "s/""${ESCAPED_QC_GEN_CONFIG_PATH}""/{{ ""${QC_CONFIG_PARAM}"" }}/g" workflows/${WF_NAME}.yaml tasks/${WF_NAME}-*
-
