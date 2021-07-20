40b0a905590f99d05ce0d6796a8522a3bebb8b32
Kostas Alexopoulos <kostorr.alex@gmail.com>
2021-07-20 13:56:57 +0200

    CPV compressor + QC (#92)
    * CPV compressor
    
    * CPV compressor QC
    
    * s/cpv-compressor-qc/cpv-qc-compressor
    
    * s/cpv-compressor-qc/cpv-qc-compressor pass 2

--- scripts/generate-all-dpl-workflows.sh
+++ scripts/generate-all-dpl-workflows.sh
@@ -24,6 +24,8 @@ function check_pwd() {
 check_pwd
 
 # ./datasampling-02.sh # this is a test workflow, no need for it in production
+./cpv-compressor.sh
+./cpv-qc-compressor.sh
 ./ft0-digits-qc.sh
 ./emc-qcmn-epn.sh
 ./hmpid-raw-qcmn.sh
