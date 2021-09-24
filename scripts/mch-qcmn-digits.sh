bb19f3d0938ef3f578ef35c5cf7b955191891606
Barthelemy <barthelemy.von.haller@cern.ch>
2021-09-24 11:34:16 +0200

    add remote machine

--- scripts/mch-qcmn-digits.sh
+++ scripts/mch-qcmn-digits.sh
@@ -66,4 +66,4 @@ ESCAPED_QC_GEN_CONFIG_PATH=$(printf '%s\n' "$QC_GEN_CONFIG_PATH" | sed -e 's/[]\
 sed -i "s/""${ESCAPED_QC_GEN_CONFIG_PATH}""/{{ ""${QC_CONFIG_PARAM}"" }}/g" workflows/${WF_NAME}.yaml tasks/${WF_NAME}-*
 
 add_fmq_shmmonitor_role workflows/${WF_NAME}.yaml
-add_qc_remote_machine_attribute workflows/${WF_NAME}.yaml alio2-cr1-qc01
+add_qc_remote_machine_attribute workflows/${WF_NAME}.yaml alio2-cr1-qme07
