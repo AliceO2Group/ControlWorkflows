6a625ab47e722548f15ef73e2ade517ece34e56e
afurs <afurs@cern.ch>
2021-09-29 16:54:36 +0200

    FDD and FV0: pipeline for readout-proxy

--- scripts/fdd-digits-pipe.sh
+++ scripts/fdd-digits-pipe.sh
@@ -11,7 +11,7 @@ cd ..
 # Generate the AliECS workflow and task templates
 o2-dpl-raw-proxy -b --session default \
   --dataspec 'A1:FDD/RAWDATA;dd:FLP/DISTSUBTIMEFRAME/0' \
-  --readout-proxy '--channel-config "name=readout-proxy,type=pull,method=connect,address=ipc:///tmp/stf-builder-dpl-pipe-0,transport=shmem,rateLogging=10"' \
+  --readout-proxy '--channel-config "name=readout-proxy,type=pull,method=connect,address=ipc:///tmp/stf-builder-dpl-pipe-0,transport=shmem,rateLogging=10"' --pipeline  readout-proxy:$N_PIPELINES \
   | o2-fdd-flp-dpl-workflow -b --session default --disable-root-output --pipeline  fdd-datareader-dpl:$N_PIPELINES\
   | o2-dpl-output-proxy -b --session default --dataspec 'digits:FDD/DIGITSBC/0;channels:FDD/DIGITSCH/0;dd:FLP/DISTSUBTIMEFRAME/0' \
   --dpl-output-proxy '--channel-config "name=downstream,type=push,method=bind,address=ipc:///tmp/stf-pipe-0,rateLogging=10,transport=shmem"' \
