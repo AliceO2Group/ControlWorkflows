eaf8d2b42325ba3104b1c39d8f7be77db85b3be6
root <root@alio2-cr1-hv-aliecs.cern.ch>
2021-10-13 15:21:46 +0200

    Add missing changes for FLP Suite v0.31.0

--- scripts/mft-raw-digits-qc.sh
+++ scripts/mft-raw-digits-qc.sh
@@ -12,7 +12,7 @@ QC_CONFIG_PARAM='qc_config_uri'
 cd ..
 
 # DPL command to generate the AliECS dump
-o2-dpl-raw-proxy -b --session default --dataspec 'x:MFT/RAWDATA;dd:FLP/DISTSUBTIMEFRAME/0' --readout-proxy '--channel-config "name=readout-proxy,type=pull,method=connect,address=ipc:///tmp/stf-builder-dpl-pipe-0,transport=shmem,rateLogging=10"' | o2-itsmft-stf-decoder-workflow -b --runmft --noise-file /tmp/_DUMMY_ --dict-file /tmp/_DUMMY_ --digits --no-clusters | o2-dpl-output-proxy -b --session default --dataspec 'x:MFT/RAWDATA;dd:FLP/DISTSUBTIMEFRAME/0' --dpl-output-proxy '--channel-config "name=downstream,type=push,method=bind,address=ipc:///tmp/stf-pipe-0,rateLogging=10,transport=shmem"' | o2-qc --config ${QC_GEN_CONFIG_PATH} -b --o2-control $WF_NAME
+o2-dpl-raw-proxy -b --session default --dataspec 'x:MFT/RAWDATA;dd:FLP/DISTSUBTIMEFRAME/0' --readout-proxy '--channel-config "name=readout-proxy,type=pull,method=connect,address=ipc:///tmp/stf-builder-dpl-pipe-0,transport=shmem,rateLogging=10"' | o2-itsmft-stf-decoder-workflow -b --runmft --digits --no-clusters --configKeyValues "MFTClustererParam.noiseFilePath=/tmp/_DUMMY_;MFTClustererParam.dictFilePath=/tmp/_DUMMY_" | o2-dpl-output-proxy -b --session default --dataspec 'x:MFT/RAWDATA;dd:FLP/DISTSUBTIMEFRAME/0' --dpl-output-proxy '--channel-config "name=downstream,type=push,method=bind,address=ipc:///tmp/stf-pipe-0,rateLogging=10,transport=shmem"' | o2-qc --config ${QC_GEN_CONFIG_PATH} -b --o2-control $WF_NAME
 
 # add the templated QC config file path
 ESCAPED_QC_FINAL_CONFIG_PATH=$(printf '%s\n' "$QC_FINAL_CONFIG_PATH" | sed -e 's/[\/&]/\\&/g')
