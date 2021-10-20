b9a3a1fc120996db235adc485d62c8a6927ade5f
Piotr Konopka <piotr.jan.konopka@cern.ch>
2021-10-20 16:38:38 +0200

    TPC: Use 128GB ram machine and 90GB of shmem

--- scripts/tpc-full-qcmn.sh
+++ scripts/tpc-full-qcmn.sh
@@ -24,6 +24,8 @@ sed -i /defaults:/\ a\\\ \\\ "${QC_CONFIG_PARAM}":\ \""${ESCAPED_QC_FINAL_CONFIG
 ESCAPED_QC_GEN_CONFIG_PATH=$(printf '%s\n' "$QC_GEN_CONFIG_PATH" | sed -e 's/[]\/$*.^[]/\\&/g');
 sed -i "s/""${ESCAPED_QC_GEN_CONFIG_PATH}""/{{ ""${QC_CONFIG_PARAM}"" }}/g" workflows/${WF_NAME}.yaml tasks/${WF_NAME}-*
 
+sed -i "s/shm_segment_size: \([0-9]\+\)/shm_segment_size: 90000000000/g" workflows/tpc-full-qcmn-remote.yaml
+
 add_fmq_shmmonitor_role workflows/${WF_NAME}.yaml
-add_qc_remote_machine_attribute workflows/${WF_NAME}.yaml alio2-cr1-qme01
+add_qc_remote_machine_attribute workflows/${WF_NAME}.yaml alio2-cr1-qts01
 
