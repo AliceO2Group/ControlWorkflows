0d72825942fd5fb58600c8d8c1cdc14ed39662c6
afurs <afurs@cern.ch>
2021-10-21 20:51:41 +0200

    FDD:new workflows

---
+++ scripts/fdd-qcmn-remote.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#set -x;
+set -e
+set -u
+
+# shellcheck disable=SC1091
+source helpers.sh
+
+QC_GEN_CONFIG_PATH="json://${PWD}/etc/fdd-qcmn.json"
+QC_FINAL_CONFIG_PATH="consul-json://{{ consul_endpoint }}/o2/components/qc/ANY/any/fdd-qcmn"
+QC_CONFIG_PARAM="qc_config_uri"
+
+WF_NAME=fdd-qcmn-remote
+
+cd ..
+
+o2-qc --config "$QC_GEN_CONFIG_PATH" --remote -b --o2-control "$WF_NAME"
+
+add_config_variable "$QC_FINAL_CONFIG_PATH" "$QC_GEN_CONFIG_PATH" "$QC_CONFIG_PARAM" "$WF_NAME"
+
+add_fmq_shmmonitor_role workflows/${WF_NAME}.yaml
+add_qc_remote_machine_attribute workflows/${WF_NAME}.yaml alio2-cr1-qme02
