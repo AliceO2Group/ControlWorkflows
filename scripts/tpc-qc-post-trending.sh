40f2bf6954770b8ec7931ccd2b527a4e5b5d8f4f
Piotr Konopka <piotr.jan.konopka@cern.ch>
2022-01-07 09:37:30 +0100

    TPC QC postprocessing workflow

---
+++ scripts/tpc-qc-post-trending.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# set -x;
+set -e;
+set -u;
+
+QC_GEN_CONFIG_PATH='json://'`pwd`'/etc/tpc-qc-post-trending.json'
+QC_FINAL_CONFIG_PATH='consul-json://{{ consul_endpoint }}/o2/components/qc/ANY/any/tpc-qc-post-trending'
+QC_CONFIG_PARAM='qc_config_uri'
+
+source helpers.sh
+
+cd ../
+
+WF_NAME=tpc-qc-post-trending-remote
+
+o2-qc --config $QC_GEN_CONFIG_PATH --remote -b --o2-control $WF_NAME
+
+# add the templated QC config file path
+ESCAPED_QC_FINAL_CONFIG_PATH=$(printf '%s\n' "$QC_FINAL_CONFIG_PATH" | sed -e 's/[\/&]/\\&/g')
+sed -i /defaults:/\ a\\\ \\\ "${QC_CONFIG_PARAM}":\ \""${ESCAPED_QC_FINAL_CONFIG_PATH}"\" workflows/${WF_NAME}.yaml
+
+# find and replace all usages of the QC config path which was used to generate the workflow
+ESCAPED_QC_GEN_CONFIG_PATH=$(printf '%s\n' "$QC_GEN_CONFIG_PATH" | sed -e 's/[]\/$*.^[]/\\&/g');
+sed -i "s/""${ESCAPED_QC_GEN_CONFIG_PATH}""/{{ ""${QC_CONFIG_PARAM}"" }}/g" workflows/${WF_NAME}.yaml tasks/${WF_NAME}-*
