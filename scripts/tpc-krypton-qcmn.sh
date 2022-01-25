150160755c4159c12bca5f1fff113e257b17f343
Robert Helmut Munzer <rmunzer@epn102.internal>
2022-01-25 10:57:23 +0100

    Change name of krypton workflow

--- scripts/tpc-krypton-qcmn.sh
+++ scripts/tpc-krypton-qcmn.sh
@@ -12,7 +12,7 @@ source helpers.sh
 
 cd ../
 
-WF_NAME=tpc-full-qcmn-krypton-remote
+WF_NAME=tpc-krypton-qcmn-remote
 
 o2-qc --config $QC_GEN_CONFIG_PATH --remote -b --o2-control $WF_NAME
 
@@ -24,7 +24,7 @@ sed -i /defaults:/\ a\\\ \\\ "${QC_CONFIG_PARAM}":\ \""${ESCAPED_QC_FINAL_CONFIG
 ESCAPED_QC_GEN_CONFIG_PATH=$(printf '%s\n' "$QC_GEN_CONFIG_PATH" | sed -e 's/[]\/$*.^[]/\\&/g');
 sed -i "s/""${ESCAPED_QC_GEN_CONFIG_PATH}""/{{ ""${QC_CONFIG_PARAM}"" }}/g" workflows/${WF_NAME}.yaml tasks/${WF_NAME}-*
 
-sed -i "s/shm_segment_size: \([0-9]\+\)/shm_segment_size: 90000000000/g" workflows/tpc-full-qcmn-krypton-remote.yaml
+sed -i "s/shm_segment_size: \([0-9]\+\)/shm_segment_size: 90000000000/g" workflows/tpc-krypton-qcmn-remote.yaml
 
 add_fmq_shmmonitor_role workflows/${WF_NAME}.yaml
 add_qc_remote_machine_attribute workflows/${WF_NAME}.yaml alio2-cr1-qts01
