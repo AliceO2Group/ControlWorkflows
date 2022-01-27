5ef9c4365a233ebeab2d7912f44369ee4167edd3
Robert Helmut Munzer <rmunzer@epn102.internal>
2022-01-27 16:03:45 +0100

    workflow test 2

--- scripts/tpc-idc.sh
+++ scripts/tpc-idc.sh
@@ -36,6 +36,10 @@ ARGS_ALL="-b --session default --shm-segment-size $GLOBAL_SHMSIZE"
 o2-dpl-raw-proxy -b --session default \
   --dataspec "${PROXY_INSPEC}" \
   --readout-proxy '--channel-config "name=readout-proxy,type=pull,method=connect,address=ipc:///tmp/stf-builder-dpl-pipe-0,transport=shmem,rateLogging=10"' \
+  | o2-tpc-idc-to-vector $ARGS_ALL \
+  --crus ${CRUS} \
+  --pedestal-file $pedestalFile \
+  --severity info \
 | o2-dpl-output-proxy -b --session default \
 --dataspec "${OUTSPEC}" \
 --dpl-output-proxy '--channel-config "name=downstream,type=push,method=bind,address=ipc:///tmp/stf-pipe-0,rateLogging=10,transport=shmem"' \
