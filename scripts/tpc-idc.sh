14e941ab42c5888f48310ad7a7f3d84f8c8ca2b0
Robert Helmut Munzer <rmunzer@epn102.internal>
2022-01-27 15:17:25 +0100

    Modified INSPEC and onput proxy

--- scripts/tpc-idc.sh
+++ scripts/tpc-idc.sh
@@ -13,8 +13,10 @@ cd ..
 #| o2-dpl-output-proxy -b --session default --dataspec 'x:ZYX/RAWDATA;dd:FLP/DISTSUBTIMEFRAME/0' --dpl-output-proxy '--channel-config "name=downstream,type=push,method=bind,address=ipc:///tmp/stf-pipe-0,rateLogging=10,transport=shmem"' --o2-control $WF_NAME
 
 export GLOBAL_SHMSIZE=$(( 16 << 30 )) #  GB for the global SHMEM
-PROXY_INSPEC="A:TPC/RAWDATA"
+#PROXY_INSPEC="A:TPC/RAWDATA"
+PROXY_INSPEC="A:TPC/RAWDATA;dd:FLP/DISTSUBTIMEFRAME/0"
 
+#OUTSPEC='downstream:TPC/1DIDC;downstream:TPC/IDCGROUP'
 OUTSPEC='downstream:TPC/1DIDC;downstream:TPC/IDCGROUP'
 # TODO: Adjust path to pedestal file
 pedestalFile="/home/tpc/IDCs/FLP/Pedestals.root"
@@ -31,7 +33,7 @@ ARGS_ALL="-b --session default --shm-segment-size $GLOBAL_SHMSIZE"
 
 o2-dpl-raw-proxy $ARGS_ALL \
   --dataspec "$PROXY_INSPEC" \
-  --readout-proxy '--channel-config "name=readout-proxy,type=pull,method=connect,address=ipc://@tf-builder-pipe-0,transport=shmem,rateLogging=1"' \
+  --readout-proxy '--channel-config "name=readout-proxy,type=pull,method=connect,address=ipc://tmp/stf-builder-pipe-0,transport=shmem,rateLogging=1"' \
   | o2-tpc-idc-to-vector $ARGS_ALL \
   --crus ${CRUS} \
   --pedestal-file $pedestalFile \
@@ -45,6 +47,7 @@ o2-dpl-raw-proxy $ARGS_ALL \
   --dataspec "${OUTSPEC}" \
   --o2-control $WF_NAME
 
+#  --readout-proxy '--channel-config "name=readout-proxy,type=pull,method=connect,address=ipc://@tf-builder-pipe-0,transport=shmem,rateLogging=1"' \
 
 
 sed -i "s/ZYX/{{ detector }}/g" workflows/${WF_NAME}.yaml tasks/${WF_NAME}-*
