fc8a4ee9fe74ad19a557726e1ddc9510ba757099
Robert Muenzer <robert.muenzer@cern.ch>
2022-04-07 10:59:56 +0200

    Update qc trending only with Laser trending calibration pp is in qc remote workflow Added also new tpc qc remote workflow which includes pp.

--- scripts/generate-all-dpl-workflows.sh
+++ scripts/generate-all-dpl-workflows.sh
@@ -106,8 +106,12 @@ check_pwd
 ./tof-compressor.sh
 ./tof-qcmn-compressor.sh
 ./tof-full-qcmn.sh
+./tpc-full-calib-qcmn.sh
 ./tpc-full-qcmn.sh
+./tpc-full-nodummy-qcmn.sh
+./tpc-full-nodummy-qcmn-pp.sh
 ./tpc-krypton-qcmn.sh
 ./tpc-qc-post-trending.sh
 ./tpc-qc-post-calib.sh
 ./tpc-qc-post-processing.sh
+
