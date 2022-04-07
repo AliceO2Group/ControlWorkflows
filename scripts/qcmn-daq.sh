298ec9604e894634bb08f003f29cdd31c3d27135
Barthélémy von Haller <barthelemy.von.haller@gmail.com>
2022-04-07 14:52:41 +0200

    Update qcmn-daq.sh

--- scripts/qcmn-daq.sh
+++ scripts/qcmn-daq.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -x;
+#set -x;
 set -e;
 set -u;
 
@@ -47,12 +47,6 @@ sed -i /defaults:/\ a\\\ \\\ "${QC_CONFIG_PARAM}":\ \""${ESCAPED_QC_FINAL_CONFIG
 # find and replace all usages of the QC config path which was used to generate the workflow
 ESCAPED_QC_GEN_CONFIG_PATH=$(printf '%s\n' "$QC_GEN_CONFIG_PATH" | sed -e 's/[]\/$*.^[]/\\&/g');
 sed -i "s/""${ESCAPED_QC_GEN_CONFIG_PATH}""/{{ ""${QC_CONFIG_PARAM}"" }}/g" workflows/${WF_NAME}.yaml tasks/${WF_NAME}-*
-
-# replace TST with a parameter
-#sed -i "s/TST/{{ detector }}/g" workflows/${WF_NAME}.yaml tasks/${WF_NAME}-*
-
-# add a default value for detector
-#sed -i /defaults:/\ a\\\ \\\ "detector: TST" workflows/${WF_NAME}.yaml
 
 add_fmq_shmmonitor_role workflows/${WF_NAME}.yaml
 add_qc_remote_machine_attribute workflows/${WF_NAME}.yaml alio2-cr1-qme08
