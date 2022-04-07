a6371cf802c9af3dab1a7dca12a81fd195398c49
Barthélémy von Haller <barthelemy.von.haller@cern.ch>
2022-04-07 15:41:39 +0200

    daq-qcmn: detector variable instead of TST (#286)
    * detector variable instead of TST
    
    * Update qcmn-daq.sh
    
    Co-authored-by: Nomad user <root@alio2-cr1-mvs03.cern.ch>

--- scripts/qcmn-daq.sh
+++ scripts/qcmn-daq.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# set -x;
+#set -x;
 set -e;
 set -u;
 
@@ -28,6 +28,11 @@ sed -i "s/""${ESCAPED_QC_GEN_CONFIG_PATH}""/{{ ""${QC_CONFIG_PARAM}"" }}/g" work
 
 # sed -i "s/alio2-cr1-mvs01/{{ it }}/g" workflows/${WF_NAME}.yaml tasks/${WF_NAME}-*
 
+# replace TST with a parameter
+sed -i "s/:TST\//:{{ detector }}\//g" workflows/${WF_NAME}.yaml tasks/${WF_NAME}-*
+
+# add a default value for detector
+sed -i /defaults:/\ a\\\ \\\ "detector: TST" workflows/${WF_NAME}.yaml
 
 WF_NAME=qcmn-daq-remote
 export DPL_CONDITION_BACKEND="http://127.0.0.1:8084"
@@ -44,7 +49,7 @@ ESCAPED_QC_GEN_CONFIG_PATH=$(printf '%s\n' "$QC_GEN_CONFIG_PATH" | sed -e 's/[]\
 sed -i "s/""${ESCAPED_QC_GEN_CONFIG_PATH}""/{{ ""${QC_CONFIG_PARAM}"" }}/g" workflows/${WF_NAME}.yaml tasks/${WF_NAME}-*
 
 add_fmq_shmmonitor_role workflows/${WF_NAME}.yaml
-add_qc_remote_machine_attribute workflows/${WF_NAME}.yaml alio2-cr1-qme08 
+add_qc_remote_machine_attribute workflows/${WF_NAME}.yaml alio2-cr1-qme08
 
 
 
