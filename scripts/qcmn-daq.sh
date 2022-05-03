77950d8ec566af9897286fa02fd90a8e014ce7a8
Piotr Konopka <piotr.jan.konopka@cern.ch>
2022-05-03 09:06:33 +0200

    larger qcmn-daq-remote

--- scripts/qcmn-daq.sh
+++ scripts/qcmn-daq.sh
@@ -10,7 +10,7 @@ WF_NAME=qcmn-daq-local
 export DPL_CONDITION_BACKEND="http://127.0.0.1:8084"
 DPL_PROCESSING_CONFIG_KEY_VALUES="NameConf.mCCDBServer=http://127.0.0.1:8084;"
 QC_GEN_CONFIG_PATH='json://'`pwd`'/etc/qcmn-daq.json'
-QC_FINAL_CONFIG_PATH='consul-json://{{ consul_endpoint }}/o2/components/qc/ANY/any/qcmn-daq'
+QC_FINAL_CONFIG_PATH='consul-json://{{ consul_endpoint }}/o2/components/qc/ANY/any/qcmn-daq-test'
 QC_CONFIG_PARAM='qc_config_uri'
 
 cd ..
