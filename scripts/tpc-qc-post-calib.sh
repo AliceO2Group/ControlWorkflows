c523b8bfa80df6be47c7902e3bfd731ab67cc5b8
Robert Muenzer <robert.muenzer@cern.ch>
2022-06-02 11:41:02 +0200

    Change parameter handling for consul file

--- scripts/tpc-qc-post-calib.sh
+++ scripts/tpc-qc-post-calib.sh
@@ -24,10 +24,10 @@ o2-qc --config $QC_GEN_CONFIG_PATH --remote -b --o2-control $WF_NAME
 # add the templated QC config file path
 ESCAPED_QC_FINAL_CONFIG_PATH=$(printf '%s\n' "$QC_FINAL_CONFIG_PATH" | sed -e 's/[\/&]/\\&/g')
 sed -i /defaults:/\ a\\\ \\\ "${QC_JSON_FILE}":\ \""${QC_JSON_FILE_DEFAULT}"\" workflows/${WF_NAME}.yaml
-sed -i /defaults:/\ a\\\ \\\ "${QC_CONFIG_PARAM}":\ \""${ESCAPED_QC_FINAL_CONFIG_PATH}"\" workflows/${WF_NAME}.yaml
+#sed -i /defaults:/\ a\\\ \\\ "${QC_CONFIG_PARAM}":\ \""${ESCAPED_QC_FINAL_CONFIG_PATH}"\" workflows/${WF_NAME}.yaml
 
 
 
 # find and replace all usages of the QC config path which was used to generate the workflow
 ESCAPED_QC_GEN_CONFIG_PATH=$(printf '%s\n' "$QC_GEN_CONFIG_PATH" | sed -e 's/[]\/$*.^[]/\\&/g');
-sed -i "s/""${ESCAPED_QC_GEN_CONFIG_PATH}""/{{ ""${QC_CONFIG_PARAM}"" }}/g" workflows/${WF_NAME}.yaml tasks/${WF_NAME}-*
+sed -i "s/""${ESCAPED_QC_GEN_CONFIG_PATH}""/""${ESCAPED_QC_FINAL_CONFIG_PATH}""/g" workflows/${WF_NAME}.yaml tasks/${WF_NAME}-*
