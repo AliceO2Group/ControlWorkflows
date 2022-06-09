980611d1648ccea3bf7a37215fddd42705f4dd25
afurs <afurs@cern.ch>
2022-06-09 19:30:19 +0200

    FIT: workflows with pipelines, 1st preparation

--- scripts/fv0-digits-pipe-ds-qc.sh
+++ scripts/fv0-digits-pipe-ds-qc.sh
@@ -4,21 +4,21 @@ set -e; # exit on error
 set -u; # exit on undefined variable
 
 # Variables
-WF_NAME=ft0-digits-qc-ds-pipe
+WF_NAME=fv0-digits-pipe-ds-qc
 export DPL_CONDITION_BACKEND="http://127.0.0.1:8084"
 DPL_PROCESSING_CONFIG_KEY_VALUES="NameConf.mCCDBServer=http://127.0.0.1:8084;"
-QC_GEN_CONFIG_PATH='json://'`pwd`'/etc/ft0-digits-qc-ds.json'
-QC_FINAL_CONFIG_PATH='consul-json://{{ consul_endpoint }}/o2/components/qc/ANY/any/ft0-digits-qc-ds-{{ it }}'
+QC_GEN_CONFIG_PATH='json://'`pwd`'/etc/fv0-digits-qc-ds.json'
+QC_FINAL_CONFIG_PATH='consul-json://{{ consul_endpoint }}/o2/components/qc/ANY/any/fv0-digits-ds-qc-{{ it }}'
 QC_CONFIG_PARAM='qc_config_uri'
-N_PIPELINES=5
+N_PIPELINES=10
 cd ..
 
 # Generate the AliECS workflow and task templates
 o2-dpl-raw-proxy -b --session default \
-  --dataspec 'A1:FT0/RAWDATA;dd:FLP/DISTSUBTIMEFRAME/0' \
-  --readout-proxy '--channel-config "name=readout-proxy,type=pull,method=connect,address=ipc:///tmp/stf-builder-dpl-pipe-0,transport=shmem,rateLogging=10"' --pipeline readout-proxy:$N_PIPELINES \
-  | o2-ft0-flp-dpl-workflow -b --session default --disable-root-output --pipeline  ft0-datareader-dpl:$N_PIPELINES --configKeyValues "${DPL_PROCESSING_CONFIG_KEY_VALUES}" \
-  | o2-dpl-output-proxy --environment "DPL_OUTPUT_PROXY_ORDERED=1" -b --session default --dataspec 'digits:FT0/DIGITSBC/0;channels:FT0/DIGITSCH/0;dd:FLP/DISTSUBTIMEFRAME/0' \
+  --dataspec 'A1:FV0/RAWDATA;dd:FLP/DISTSUBTIMEFRAME/0' \
+  --readout-proxy '--channel-config "name=readout-proxy,type=pull,method=connect,address=ipc:///tmp/stf-builder-dpl-pipe-0,transport=shmem,rateLogging=10"' \
+  | o2-fv0-flp-dpl-workflow -b --session default --disable-root-output --pipeline  fv0-datareader-dpl:$N_PIPELINES --configKeyValues "${DPL_PROCESSING_CONFIG_KEY_VALUES}" \
+  | o2-dpl-output-proxy --environment "DPL_OUTPUT_PROXY_ORDERED=1" -b --session default --dataspec 'digits:FV0/DIGITSBC/0;channels:FV0/DIGITSCH/0;dd:FLP/DISTSUBTIMEFRAME/0' \
   --dpl-output-proxy '--channel-config "name=downstream,type=push,method=bind,address=ipc:///tmp/stf-pipe-0,rateLogging=10,transport=shmem"' \
   | o2-qc --config ${QC_GEN_CONFIG_PATH} -b \
   --o2-control $WF_NAME
