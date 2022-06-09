358dbfcecb393d9efba13d9931388fc9c62ef355
Piotr Konopka <piotr.jan.konopka@cern.ch>
2022-06-09 16:04:01 +0200

    [OCTRL-665] Add DPL_OUTPUT_PROXY_ORDERED to each dpl-output-proxy (#366)

--- scripts/ft0-decoder-pipe-ds-qc.sh
+++ scripts/ft0-decoder-pipe-ds-qc.sh
@@ -4,24 +4,23 @@ set -e; # exit on error
 set -u; # exit on undefined variable
 
 # Variables
-WF_NAME=ft0-decoder-pipe-ds-qc
+WF_NAME=ft0-digits-qc-ds
 export DPL_CONDITION_BACKEND="http://127.0.0.1:8084"
-export DPL_CONDITION_QUERY_RATE="${GEN_TOPO_EPN_CCDB_QUERY_RATE:--1}"
 DPL_PROCESSING_CONFIG_KEY_VALUES="NameConf.mCCDBServer=http://127.0.0.1:8084;"
-QC_GEN_CONFIG_PATH='json://'`pwd`'/etc/ft0-digits-ds-qc.json'
-QC_FINAL_CONFIG_PATH='consul-json://{{ consul_endpoint }}/o2/components/qc/ANY/any/ft0-digits-ds-qc-{{ it }}'
+QC_GEN_CONFIG_PATH='json://'`pwd`'/etc/ft0-digits-qc-ds.json'
+QC_FINAL_CONFIG_PATH='consul-json://{{ consul_endpoint }}/o2/components/qc/ANY/any/'${WF_NAME}'-{{ it }}'
 QC_CONFIG_PARAM='qc_config_uri'
-N_PIPELINES=7
+
 cd ..
 
 # Generate the AliECS workflow and task templates
 o2-dpl-raw-proxy -b --session default \
   --dataspec 'A1:FT0/RAWDATA;dd:FLP/DISTSUBTIMEFRAME/0' \
   --readout-proxy '--channel-config "name=readout-proxy,type=pull,method=connect,address=ipc:///tmp/stf-builder-dpl-pipe-0,transport=shmem,rateLogging=10"' \
-  | o2-ft0-flp-dpl-workflow -b --session default --disable-root-output --new-decoder --pipeline  ft0-datadecoder-dpl:$N_PIPELINES --configKeyValues "${DPL_PROCESSING_CONFIG_KEY_VALUES}" \
+  | o2-ft0-flp-dpl-workflow -b --session default --disable-root-output \
   | o2-dpl-output-proxy --environment "DPL_OUTPUT_PROXY_ORDERED=1" -b --session default --dataspec 'digits:FT0/DIGITSBC/0;channels:FT0/DIGITSCH/0;dd:FLP/DISTSUBTIMEFRAME/0' \
   --dpl-output-proxy '--channel-config "name=downstream,type=push,method=bind,address=ipc:///tmp/stf-pipe-0,rateLogging=10,transport=shmem"' \
-  | o2-qc --config ${QC_GEN_CONFIG_PATH} -b --configKeyValues "${DPL_PROCESSING_CONFIG_KEY_VALUES}" \
+  | o2-qc --config ${QC_GEN_CONFIG_PATH} -b \
   --o2-control $WF_NAME
 # Add the final QC config file path as a variable in the workflow template
 ESCAPED_QC_FINAL_CONFIG_PATH=$(printf '%s\n' "$QC_FINAL_CONFIG_PATH" | sed -e 's/[\/&]/\\&/g')
