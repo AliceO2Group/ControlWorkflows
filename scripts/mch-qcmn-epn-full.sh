8e602fa0b232870834eaaa9736ffa3eb3c9b9af4
aferrero2707 <aferrero1975@gmail.com>
2022-06-15 16:56:08 +0200

    [MCH] added tasks for ROFs and preclusters analysis

---
+++ scripts/mch-qcmn-epn-full.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#set -x;
+set -e
+set -u
+
+# shellcheck disable=SC1091
+source helpers.sh
+
+QC_GEN_CONFIG_PATH="json://${PWD}/etc/mch-qcmn-epn-full.json"
+QC_FINAL_CONFIG_PATH="consul-json://{{ consul_endpoint }}/o2/components/qc/ANY/any/mch-qcmn-epn-full"
+QC_CONFIG_PARAM="qc_config_uri"
+
+WF_NAME=mch-qcmn-epn-full-remote
+export DPL_CONDITION_BACKEND="http://127.0.0.1:8084"
+DPL_PROCESSING_CONFIG_KEY_VALUES="NameConf.mCCDBServer=http://127.0.0.1:8084;"
+
+cd ..
+
+o2-qc --config "$QC_GEN_CONFIG_PATH" --remote -b --o2-control "$WF_NAME"
+
+add_config_variable "$QC_FINAL_CONFIG_PATH" "$QC_GEN_CONFIG_PATH" "$QC_CONFIG_PARAM" "$WF_NAME"
+
