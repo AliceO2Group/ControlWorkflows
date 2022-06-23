7a45b2aa685785443d2f4e65bb59c4f9dbcefc46
rmunzer <97919772+rmunzer@users.noreply.github.com>
2022-06-23 13:58:30 +0200

    TPC- Integrated digital current workflow (#373)
    * Basic code for tpc-idc.sh tpc-idc-merger.sh
    
    * remove fixed node
    
    * add more lanes to merger
    
    * fix output proxy specs
    
    * Reduced lanes to 5
    
    * Remmove 1DIDC
    
    * Add missing $
    
    * Add disabeld CCDB
    
    * Disabled CCDB also in merger
    
    * Set all verbosity to warning
    
    * disable workflow for flp145
    
    * disable workflow for flp145
    
    * disable workflow for flp145
    
    * disable workflow for flp145
    
    * disable workflow for flp145
    
    * disable workflow for flp145
    
    * disable workflow for flp145
    
    * disable workflow for flp145
    
    * Combined idc-merger with qc-merger
    
    * add automatic disbale in workflow creation
    
    * Redo nodummy-qcmn
    
    * correct if statement
    
    * changed synteax in if statement
    
    * changed synteax in if statement
    
    * change in enabled syntax
    
    * change in enabled syntax
    
    * change in enabled syntax
    
    * change in enabled syntax
    
    * change in enabled syntax
    
    * change in enabled syntax
    
    * change in enabled syntax
    
    * change in enabled syntax
    
    * change in enabled syntax
    
    * change in enabled syntax
    
    * change in enabled syntax
    
    * change in enabled syntax
    
    * change in enabled syntax
    
    * change in enabled syntax
    
    * recreate with new version
    
    * set pedestal-url option to test ccdb
    
    * remove DISTTIMEFRAME SPEC
    
    * set infologger-severity
    
    * set severity to warning for info-logger
    
    * add ccdb-populator
    
    * add ccdb-populator task
    
    * remove tasks in merger
    
    * disable info in merger
    
    * change to pub/sub
    
    * add factorize and aggregator again
    
    * add master workflow
    
    * remove :
    
    * Change workflow for flp145
    
    * remove if in tpc-idc workflow
    
    * small corrections
    
    * Change pedestal input to ali ccdb
    
    * Change path the consul file for qc process
    
    * Remove obsolete stuff from script

--- scripts/tpc-idc.sh
+++ scripts/tpc-idc.sh
@@ -1,10 +1,10 @@
-  #!/usr/bin/env bash
+ #!/usr/bin/env bash
 
 # set -x;
 set -e;
 set -u;
 
-module load O2PDPSuite
+#module load O2PDPSuite
 
 source helpers.sh
 
@@ -18,10 +18,6 @@ PROXY_INSPEC="x:TPC/RAWDATA;dd:FLP/DISTSUBTIMEFRAME/0"
 
 OUTSPEC_IDC="idc2:TPC/IDCGROUP"
 OUTSPEC="xout:TPC/RAWDATA;ddout:FLP/DISTSUBTIMEFRAME/0"
-# TODO: Adjust path to pedestal file
-pedestalFile="/home/tpc/IDCs/FLP/Pedestals.root"
-
-CRUIDS="11,13"
 
 # TODO: Adjust path and check this ends up properly in the script
 CRU_GEN_CONFIG_PATH='//'`pwd`'/scripts/etc/getCRUs.sh'
@@ -30,26 +26,24 @@ CRU_CONFIG_PARAM='cru_config_uri'
 
 CRUS='\"$(/home/tpc/IDCs/FLP/getCRUs.sh)\"'
 CRUS_LOCAL='$('`pwd`"/etc/getCRU.sh"
-#CRUS='$(/tmp/getCRUs.sh)'
+
 # TODO: Adjust merger and port, if the port is change this also must be done
 #       in the merger script
+
 MERGER=alio2-cr1-qts01.cern.ch
-#MERGER=epn028-ib
-#MERGER=alio2-cr1-flp145
 PORT=47734
 
 ARGS_ALL="-b --session default "
-#--shm-segment-size $GLOBAL_SHMSIZE"
 
 o2-dpl-raw-proxy $ARGS_ALL \
   --dataspec "$PROXY_INSPEC" \
   --readout-proxy '--channel-config "name=readout-proxy,type=pull,method=connect,address=ipc://tmp/stf-builder-dpl-pipe-0,transport=shmem,rateLogging=1"' \
   | o2-tpc-idc-to-vector $ARGS_ALL \
   --crus ${CRU_GEN_CONFIG_PATH} \
-  --pedestal-file $pedestalFile \
-  --infologger-severity warning \
-  --severity warning \
+  --severity error \
+  --infologger-severity error \
   --configKeyValues "keyval.output_dir=/dev/null" \
+  --pedestal-url http://o2-ccdb.internal \
   | o2-tpc-idc-flp $ARGS_ALL \
   --propagateIDCs true \
   --crus ${CRU_GEN_CONFIG_PATH} \
@@ -63,14 +57,14 @@ o2-dpl-raw-proxy $ARGS_ALL \
    --proxy-channel-name tpc-idc-merger-proxy \
    --labels "tpc-idc-merger-proxy:ecs-preserve-raw-channels" \
    --output-proxy-method connect \
-   --tpc-idc-merger-proxy '--channel-config "name=tpc-idc-merger-proxy,method=connect,address=tcp://{{ merger_node }}:{{ merger_port }},type=push,transport=zeromq" ' \
+   --tpc-idc-merger-proxy '--channel-config "name=tpc-idc-merger-proxy,method=connect,address=tcp://{{ merger_node }}:{{ merger_port }},type=pub,transport=zeromq" ' \
    --dataspec "${OUTSPEC_IDC}" \
    --severity warning \
-   --infologger-severity warning \
   | o2-dpl-output-proxy $ARGS_ALL \
    --dpl-output-proxy '--channel-config "name=downstream,type=push,method=bind,address=ipc:///tmp/stf-pipe-0,rateLogging=10,transport=shmem"' \
    --dataspec "${OUTSPEC}" \
    --o2-control $WF_NAME
+
 
 #   --default-port 47734 \
 #   --tpc-idc-merger-proxy '--channel-config "name=tpc-idc-merger-proxy,method=connect,type=push,transport=zeromq" ' \
@@ -123,38 +117,40 @@ echo "CRUs: ${sCRUs}"
 
 CCDB="http://ccdb-test.cern.ch:8080"
 
-crus="$firstCRU-$lastCRU"
-crus="11,13"
-
-
 
 CRU_GEN_MERGER_ID='{CRUS}'
-CRU_FINAL_MERGER_ID='0-359'
+CRU_FINAL_MERGER_ID='0-355'
 CRU_MERGER_ID='cru_merger_ids'
+
+QC_GEN_CONFIG_PATH='json://'`pwd`'/scripts/etc/tpc-full-nodummy-qcmn.json'
+QC_FINAL_CONFIG_PATH='consul-json://{{ consul_endpoint }}/o2/components/qc/ANY/any/tpc-full-nodummy-postprocessing-qcmn'
+QC_CONFIG_PARAM='qc_config_uri'
+
+export DPL_CONDITION_BACKEND="http://127.0.0.1:8084"
+DPL_PROCESSING_CONFIG_KEY_VALUES="NameConf.mCCDBServer=http://127.0.0.1:8084;"
+
 
 ARGS_ALL="-b --session default --shm-segment-size $GLOBAL_SHMSIZE"
 o2-dpl-raw-proxy $ARGS_ALL \
   --dataspec ${sCRUs} \
   --severity warning \
-  --infologger-severity warning \
   --labels "tpc-idc-merger-proxy:ecs-preserve-raw-channels" \
   --proxy-name tpc-idc-merger-proxy \
-  --channel-config "name=tpc-idc-merger-proxy,type=pull,method=bind,address=tcp://*:{{ merger_port }},rateLogging=1,transport=zeromq" \
+  --channel-config "name=tpc-idc-merger-proxy,type=sub,method=bind,address=tcp://*:{{ merger_port }},rateLogging=1,transport=zeromq" \
   | o2-tpc-idc-distribute $ARGS_ALL \
   --crus ${CRU_GEN_MERGER_ID} \
   --firstTF  1 \
   --timeframes ${nTFs} \
   --output-lanes ${lanes} \
   --configKeyValues 'keyval.output_dir=/dev/null'  \
-  --severity warning \
-  --infologger-severity warning \
+  --severity error \
+  --infologger-severity error \
   | o2-tpc-idc-factorize $ARGS_ALL \
   --crus ${CRU_GEN_MERGER_ID} \
   --timeframes ${nTFs} \
   --input-lanes ${lanes} \
   --configFile "" \
   --compression 0 \
-  --ccdb-uri "${CCDB}" \
   --configKeyValues 'TPCIDCGroupParam.groupPadsSectorEdges=32211;keyval.output_dir=/dev/null'  \
   --groupIDCs warning \
   --nthreads-grouping 4 \
@@ -163,17 +159,21 @@ o2-dpl-raw-proxy $ARGS_ALL \
   --severity warning \
   --infologger-severity warning \
   --use-approximate-timestamp true \
+  --sendOutputFFT true \
   | o2-tpc-idc-ft-aggregator $ARGS_ALL \
   --rangeIDC 200 \
   --nFourierCoeff 40 \
   --timeframes ${nTFs} \
-  --ccdb-uri "${CCDB}" \
   --configKeyValues 'keyval.output_dir=/dev/null'  \
   --severity warning \
   --infologger-severity warning \
-  --o2-control $WF_NAME
+  |  o2-calibration-ccdb-populator-workflow $ARGS_ALL \
+  --ccdb-path http://ccdb-test.cern.ch:8080 \
+  |  o2-qc --config $QC_GEN_CONFIG_PATH --remote -b --o2-control $WF_NAME
 
 
+
+#  --ccdb-uri "${CCDB}" \
 
 
 # replace {{ it  }} in in proxy name in taks and workflow
@@ -198,4 +198,15 @@ sed -i "s/""${ORIGINAL_STRING}""/""${REPLACE_STRING}""/g" workflows/${WF_NAME}.y
 
 
 add_qc_remote_machine_attribute workflows/${WF_NAME}.yaml alio2-cr1-qts01
+
+# add the templated QC config file path
+ESCAPED_QC_FINAL_CONFIG_PATH=$(printf '%s\n' "$QC_FINAL_CONFIG_PATH" | sed -e 's/[\/&]/\\&/g')
+sed -i /defaults:/\ a\\\ \\\ "${QC_CONFIG_PARAM}":\ \""${ESCAPED_QC_FINAL_CONFIG_PATH}"\" workflows/${WF_NAME}.yaml
+
+# find and replace all usages of the QC config path which was used to generate the workflow
+ESCAPED_QC_GEN_CONFIG_PATH=$(printf '%s\n' "$QC_GEN_CONFIG_PATH" | sed -e 's/[]\/$*.^[]/\\&/g');
+sed -i "s/""${ESCAPED_QC_GEN_CONFIG_PATH}""/{{ ""${QC_CONFIG_PARAM}"" }}/g" workflows/${WF_NAME}.yaml tasks/${WF_NAME}-*
+
+sed -i "s/shm_segment_size: \([0-9]\+\)/shm_segment_size: 90000000000/g" workflows/${WF_NAME}.yaml
+
 
